e15e57dd36f554bf76bbd93ed6dc085f928d4aa2
Jacob Frost <36671442+frosty656@users.noreply.github.com>
2020-11-05 13:36:51 -0500

    Added data files and start to choosing one

--- src/ml_app/app.py
+++ src/ml_app/app.py
@@ -5,7 +5,8 @@ import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW, CENTER
 import subprocess
-
+import os
+from functools import partial
 
 class MachineLearningPortal(toga.App):
 
@@ -32,21 +33,56 @@ class MachineLearningPortal(toga.App):
         button8 = toga.Button('Multi-Layer Neural Networks', style=Pack(height=20, padding=13), on_press=self.MultiLayerNeuralNetworks)
         button9 = toga.Button('Optimization with Linear Programming', style=Pack(height=20, padding=13), on_press=self.OptimizationWithLinearProgramming)
         button10 = toga.Button('Massively Parallel Programming with Spark', style=Pack(height=20, padding=13), on_press=self.MassivelyParallelProgrammingWithSpark)
-
-
+        button11 = toga.Button('Change Data Set', style=Pack(height=20, padding=13), on_press=self.changeDataset)
         scroll_box = toga.Box(children=[], style=Pack(direction=COLUMN, padding=10, flex=1))
-
-        scroll_box.add(button1, button2, button3, button4, button5, button6, button7, button8, button9, button10)
-
+        scroll_box.add(button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11)
         scroll_box.add(openJupyterButton, closeJupyterButton)
-
         scroll_view = toga.ScrollContainer(content=scroll_box, style=Pack(padding=10))
 
 
 
         main_box.add(scroll_view)
-        #main_box.add(button1, button2, button3, button4, button5, button6, button7, button8, button9, button10)
 
+
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
+        self.main_window.show()
+
+
+    def handle_dataset_selection(self, widget, data):
+        #self.data = pd.read_csv(data)
+        print(data)
+
+    def changeDataset(self, widget):
+        main_box = toga.Box(style=Pack(direction=COLUMN))
+        #TODO: WE should make this a list of buttons
+        dataFiles = [
+            (i, i.split(".")[0].replace("_", " "))
+            for i in os.listdir('./Data')
+        ]
+
+        self.left_container = toga.Box(style=Pack(direction=COLUMN, padding_top=50))
+
+        for i in dataFiles:
+            tmpbutton = toga.Button(i[1], style=Pack(width=100, height=25), on_press=partial(self.handle_dataset_selection,data=i[0]))
+            self.left_container.add(tmpbutton)
+
+        #TODO: we should change this is a datatable and have it show the data from the file selected
+        right_content = toga.Box(
+            style=Pack(direction=COLUMN, padding_top=50, flex= 5)
+        )
+
+
+
+        right_container = toga.ScrollContainer(horizontal=False)
+
+        right_container.content = right_content
+
+        split = toga.SplitContainer()
+
+        split.content = [self.left_container, right_container]
+
+        main_box.add(split)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
@@ -78,8 +114,6 @@ class MachineLearningPortal(toga.App):
         subprocess.Popen('jupyter notebook --no-browser', shell=True)
     def closeJupyterNotebook(self, widget):
         subprocess.Popen('jupyter notebook stop', shell=True)
-
-
 
 def main():
     return MachineLearningPortal()
